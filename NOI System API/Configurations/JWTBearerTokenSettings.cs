02de43f03057117fdab2cca0aaab95461e56e306
Selma Auala <219009430@students.nust.na>
2022-07-08 16:41:08 +0200

    Add project files.

---
+++ NOI System API/Configurations/JWTBearerTokenSettings.cs
@@ -0,0 +1,13 @@
+﻿#nullable disable
+
+namespace NOI_System_API.Configurations
+{
+    public class JWTBearerTokenSettings
+    {
+        public string SecretKey { get; set; }
+        public string Audience { get; set; }
+        public string Issuer { get; set; }
+        public string ExpiryTimeInSeconds { get; set; }
+    
+    }
+}
---
+++ NOI System API/Controllers/AuthenticationController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NOI_System_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register() 
+        {
+            //Business Logic
+            return await Task.FromResult(Ok("This functon was successful"));
+
+        }
+        [HttpPost]
+        [Route("Login")]
+        public IActionResult Login()
+        {
+            return BadRequest("Call Successful");
+        }
+    }
+}
