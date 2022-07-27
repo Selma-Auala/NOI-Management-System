43b1209e086a7a5df61845f53462ab5cd99ead4b
dmdmaasz6 <dmdmaasz6@users.noreply.github.com>
2022-07-27 18:59:38 +0200

    Update Solution Files - Donovan

--- NOI System API/Program.cs
+++ NOI System API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using NOI_System_API.DBContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(); 
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString(“DefaultConnection”)));
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"), ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))));
 
 var app = builder.Build();
 
--- SharedModels/Class1.cs
+++ SharedModels/Class1.cs
@@ -1,4 +1,4 @@
-﻿namespace Shared
+﻿namespace SharedModels
 {
     public class Class1
     {
